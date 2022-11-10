40b404c7bb39c7cd0236fe2d035819454a19d061
Tim <sarrtim@gmail.com>
2022-11-10 16:05:22 +0300

    Update session.py

--- app/core/session.py
+++ app/core/session.py
@@ -1,16 +1,23 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-from functools import lru_cache
 from typing import Generator
 from typing import Any
 from app.core.config import Settings
 
-Base: Any = declarative_base()
-engine = create_async_engine(Settings.database_url, echo=True, pool_pre_ping=True)
+metadata = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
+Base: Any = declarative_base(metadata=metadata)
+engine = create_engine(Settings().database_url, echo=True, pool_pre_ping=True)
 
 
-@lru_cache
 def create_session() -> scoped_session:
     Session = scoped_session(
         sessionmaker(autocommit=False, autoflush=False, bind=engine)
