29d7374af5130377ff739ca685c1b2b15dfc496f
Tim <sarrtim@gmail.com>
2022-11-10 11:04:42 +0300

    Add create_session

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from app.core.session import create_session
 
 app = FastAPI(
-    title="test",
-    description="test",
+    title="Shop API",
+    description="Shop API",
     version="0.0.1",
 )
+db = create_session()
 
 
 @app.get("/")
