6cf9c25beaf6418fe8fcb1019d37e92f4bb56954
Tim <sarrtim@gmail.com>
2022-11-10 15:41:03 +0300

    Add create migration cli

--- manage.py
+++ manage.py
@@ -3,13 +3,19 @@ import click
 
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 
 @click.group()
 def main():
     """This is a management script for the application."""
+
+
+@main.command("create_migration", short_help="Create new migration version.")
+@click.option("--message", help="Migration message")
+def create_migration(**kwargs):
+    os.system(
+        'alembic revision --autogenerate -m "{}"'.format(kwargs.get("message")))
 
 
 @main.command("run_server", short_help="Run development server.")
