72b23db7e68935b60ffc5bc339a18a1c41eeeaa9
Tim <sarrtim@gmail.com>
2022-11-10 19:28:02 +0300

    Ad BaseModel helper crud methods

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,8 @@
+import sqlalchemy
 import sqlalchemy as sa
 
 from datetime import datetime
+from starlette.exceptions import HTTPException
 from app.core.session import Base
 
 
@@ -21,3 +23,38 @@ class BaseModel(AbstractBaseModel):
     __abstract__ = True
 
     id = sa.Column(sa.Integer, primary_key=True)
+
+    def get(self, id):
+        obj = self.db.query(self.model).get(id)
+        if obj is None:
+            raise HTTPException(status_code=404, detail="Not Found")
+        return obj
+
+    def list(self):
+        objs = self.db.query(self.model).all()
+        return objs
+
+    def create(self, obj):
+        db_obj = self.model(**obj.dict())
+        self.db.add(db_obj)
+        try:
+            self.db.commit()
+        except sqlalchemy.exc.IntegrityError as e:
+            self.db.rollback()
+            if "duplicate key" in str(e):
+                raise HTTPException(status_code=409, detail="Conflict Error")
+            else:
+                raise e
+        return db_obj
+
+    def update(self, id, obj):
+        db_obj = self.get(id)
+        for column, value in obj.dict(exclude_unset=True).items():
+            setattr(db_obj, column, value)
+        self.db.commit()
+        return db_obj
+
+    def delete(self, id) -> None:
+        db_obj = self.db.query(self.model).get(id)
+        self.db.delete(db_obj)
+        self.db.commit()
