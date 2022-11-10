605cfb5bba3e060bf5225bd6fe5daa179529d9a9
Tim <sarrtim@gmail.com>
2022-11-10 13:42:17 +0300

    Add AbstractBaseModel & Base Model implmentation

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,22 @@
+import sqlalchemy as sa
+
 from app.core.session import Base
 
 
+class AbstractBaseModel(Base):
+    __abstract__ = True
+
+    created_at = sa.Column(
+        sa.DateTime(timezone=True), default=sa.func.current_timestamp(),
+        nullable=False
+    )
+    updated_at = sa.Column(
+        sa.DateTime(timezone=True), default=sa.func.current_timestamp(),
+        nullable=False, onupdate=sa.func.current_timestamp()
+    )
+
+
 class BaseModel(Base):
-    pass
+    __abstract__ = True
+
+    id = sa.Column(sa.Integer, primary_key=True)
